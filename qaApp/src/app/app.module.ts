dad9b0d1fad5a8d3f3592529b7726bdab17538dd
tushgee <tushgee@gmail.com>
2019-09-22 13:20:21 -0500

    created Analytics module with Dashboard component

--- qaApp/src/app/app.module.ts
+++ qaApp/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// import the feature module here so you can add it to the imports array below
+import { AnalyticsModule } from './analytics/analytics.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    AnalyticsModule // feature module
   ],
   providers: [],
   bootstrap: [AppComponent]
